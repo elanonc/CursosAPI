1379278c1aacf2d5743d45a742f65661b127d671
elanonc <nunes.elano14@gmail.com>
2023-12-19 00:35:50 -0300

    config: ajustes em configs e scrip de criar tabelas no postgres

--- core/configs.py
+++ core/configs.py
@@ -1,15 +1,16 @@
-from pydantic import BaseSettings
+from pydantic_settings import BaseSettings
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import DeclarativeMeta
 
 class Settings(BaseSettings):
     """
         Configurações gerais usadas na aplicação
     """
-API_V1_STR: str = '/api/v1'
-DB_URL: str = "postgresql+asyncpg://elanonc:e14092000@localhost:5432/faculdade"
-DBBaseModel = declarative_base()
+    API_V1_STR: str = '/api/v1'
+    DB_URL: str = "postgresql+asyncpg://postgres:postgres@localhost:5432/cursos"
+    DBBaseModel: DeclarativeMeta = declarative_base()
 
-class Config:
-    case_sensitive: True
+    class Config:
+        case_sensitive: True
 
 settings = Settings()
